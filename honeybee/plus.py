1162ccc206820e15e948ae8ec018cb9c1dd9fb8d
LAPTOP-MB0HU4QI\Mostapha <sadeghipour@gmail.com>
2017-12-12 15:03:18 -0500

    Updated plus.py library

--- honeybee/plus.py
+++ honeybee/plus.py
@@ -12,7 +12,7 @@ except ImportError as e:
 #  The stucture of argumnets and retuens should stay the same.
 # --------------------------------------------------------------------------------------
 # TODO(someone!): Implement triangulate
-def extract_geometry_points(geometries, meshing_parameters=None):
+def extractGeometryPoints(geometries, meshingParameters=None):
     """Calculate list of points for a Grasshopper geometry.
 
     For planar surfaces the length of the list will be only 1. For non-planar
@@ -22,7 +22,7 @@ def extract_geometry_points(geometries, meshing_parameters=None):
         geometries: List of meshes or Breps
         triangulate: If set to True the function returns the points for triangulated
             surfaces (Default: False)
-        meshing_parameters: Optional Rhino MeshingParameters. This will only be used if
+        meshingParameters: Optional Rhino meshingParameters. This will only be used if
             the surface is non-planar or has an internal edge and needs to be meshed.
             Default:
                 Rhino.Geometry.MeshingParameters.Coarse; SimplePlanes = True for planar
@@ -39,45 +39,45 @@ def extract_geometry_points(geometries, meshing_parameters=None):
     for geometry in geometries:
 
         if isinstance(geometry, rc.Geometry.Mesh):
-            yield extract_mesh_points((geometry,))
+            yield extractMeshPoints((geometry,))
         elif isinstance(geometry, rc.Geometry.Brep):
-            yield extract_brep_points(geometry, meshing_parameters)
+            yield extractBrepPoints(geometry, meshingParameters)
         else:
             raise TypeError(
                 'Input surface should be a Mesh or a Brep not {}.'.format(type(geometry))
             )
 
 
-def xyz_to_geometrical_points(xyz_points):
+def xyzToGeometricalPoints(xyzPoints):
     """convert a sequence of (x, y, z) values to Grasshopper points.
 
     Input should be list of lists of points.
     """
-    for xyz_list in xyz_points:
-        for xyz in xyz_list:
+    for xyzList in xyzPoints:
+        for xyz in xyzList:
             yield rc.Geometry.Point3d(xyz[0], xyz[1], xyz[2])
 
 
-def polygon(point_list):
+def polygon(pointList):
     """Return a polygon from points."""
-    return rc.Geometry.Polyline(point_list).ToNurbsCurve()
+    return rc.Geometry.Polyline(pointList).ToNurbsCurve()
 
 
 # ------------------------- End of honeybee[+] methods -----------------------------
 # ------------------------------ Utilities -----------------------------------------
-def is_planar(geometry, tol=1e-3):
+def isPlanar(geometry, tol=1e-3):
     """Check if a surface in planar."""
     return geometry.Faces[0].IsPlanar(tol)
 
 
-def extract_brep_points(brep, meshing_parameters=None, tol=1e-3):
+def extractBrepPoints(brep, meshingParameters=None, tol=1e-3):
     """Extract points from Brep."""
-    meshing_parameters = meshing_parameters or rc.Geometry.MeshingParameters.Coarse
+    meshingParameters = meshingParameters or rc.Geometry.MeshingParameters.Coarse
     for fid in xrange(brep.Faces.Count):
         geometry = brep.Faces[fid].DuplicateFace(False)
         if not brep.Faces[fid].IsPlanar(tol):
-            meshes = rc.Geometry.Mesh.CreateFromBrep(geometry, meshing_parameters)
-            yield next(extract_mesh_points(meshes))
+            meshes = rc.Geometry.Mesh.CreateFromBrep(geometry, meshingParameters)
+            yield next(extractMeshPoints(meshes))
         else:
             # planar surface
             pts = geometry.DuplicateVertices()
@@ -91,26 +91,26 @@ def extract_brep_points(brep, meshing_parameters=None, tol=1e-3):
 
             if len(border) > 1:
                 # mesh the surface
-                meshing_parameters.SimplePlanes = True
-                meshes = rc.Geometry.Mesh.CreateFromBrep(geometry, meshing_parameters)
-                yield next(extract_mesh_points(meshes))
+                meshingParameters.SimplePlanes = True
+                meshes = rc.Geometry.Mesh.CreateFromBrep(geometry, meshingParameters)
+                yield next(extractMeshPoints(meshes))
             else:
                 # In some strange cases Rhino returns a single point for the surface
                 if len(pts) == 1:
                     pts = (p.Location for p in border[0].Points)
 
-                points_sorted = sorted(pts, key=lambda pt: border[0].ClosestPoint(pt)[1])
+                pointsSorted = sorted(pts, key=lambda pt: border[0].ClosestPoint(pt)[1])
                 # make sure points are anti clockwise
-                if not is_points_sorted_anti_clockwise(
-                        points_sorted,
-                        get_surface_center_ptand_normal(geometry).normal_vector):
-                    points_sorted.reverse()
+                if not isPointsSortedAntiClockwise(
+                        pointsSorted,
+                        getSurfaceCenterPtandNormal(geometry).normalVector):
+                    pointsSorted.reverse()
                 # return sorted points
                 # Wrap in a list as Honeybee accepts list of list of points
-                yield geometry, (points_sorted,)
+                yield geometry, (pointsSorted,)
 
 
-def extract_mesh_points(meshes):
+def extractMeshPoints(meshes):
     """Extract points from a mesh."""
     for mesh in meshes:
         yield mesh, tuple(
@@ -121,77 +121,77 @@ def extract_mesh_points(meshes):
         )
 
 
-def vectors_cross_product(vector1, vector2):
+def vectorsCrossProduct(vector1, vector2):
     """Calculate cross product of two vectors."""
     return vector1.X * vector2.X + \
         vector1.Y * vector2.Y + vector1.Z * vector2.Z
 
 
-def is_points_sorted_anti_clockwise(sorted_points, normal):
+def isPointsSortedAntiClockwise(sortedPoints, normal):
     """Check if an ordered list of points are anti-clockwise."""
-    vector0 = rc.Geometry.Vector3d(sorted_points[1] - sorted_points[0])
-    vector1 = rc.Geometry.Vector3d(sorted_points[-1] - sorted_points[0])
-    pts_normal = rc.Geometry.Vector3d.CrossProduct(vector0, vector1)
+    vector0 = rc.Geometry.Vector3d(sortedPoints[1] - sortedPoints[0])
+    vector1 = rc.Geometry.Vector3d(sortedPoints[-1] - sortedPoints[0])
+    ptsNormal = rc.Geometry.Vector3d.CrossProduct(vector0, vector1)
 
     # in case points are anti-clockwise then normals should be parallel
-    if vectors_cross_product(pts_normal, normal) > 0:
+    if vectorsCrossProduct(ptsNormal, normal) > 0:
         return True
     else:
         return False
 
 
-def get_surface_center_ptand_normal(geometry):
-    """Calculate center point and normal for a hb_surface.
+def getSurfaceCenterPtandNormal(geometry):
+    """Calculate center point and normal for a HBSurface.
 
     Args:
-        hb_surface: A Honeybee surface
+        HBSurface: A Honeybee surface
 
     Returns:
-        Returns a tuple as (center_pt, normal_vector)
+        Returns a tuple as (centerPt, normalVector)
     """
-    brep_face = geometry.Faces[0]
-    if brep_face.IsPlanar and brep_face.IsSurface:
-        u_domain = brep_face.Domain(0)
-        v_domain = brep_face.Domain(1)
-        center_u = (u_domain.Min + u_domain.Max) / 2
-        center_v = (v_domain.Min + v_domain.Max) / 2
+    brepFace = geometry.Faces[0]
+    if brepFace.IsPlanar and brepFace.IsSurface:
+        u_domain = brepFace.Domain(0)
+        v_domain = brepFace.Domain(1)
+        centerU = (u_domain.Min + u_domain.Max) / 2
+        centerV = (v_domain.Min + v_domain.Max) / 2
 
-        center_pt = brep_face.PointAt(center_u, center_v)
-        normal_vector = brep_face.NormalAt(center_u, center_v)
+        centerPt = brepFace.PointAt(centerU, centerV)
+        normalVector = brepFace.NormalAt(centerU, centerV)
     else:
-        centroid = rc.Geometry.AreaMassProperties.Compute(brep_face).Centroid
-        uv = brep_face.ClosestPoint(centroid)
-        center_pt = brep_face.PointAt(uv[1], uv[2])
-        normal_vector = brep_face.NormalAt(uv[1], uv[2])
+        centroid = rc.Geometry.AreaMassProperties.Compute(brepFace).Centroid
+        uv = brepFace.ClosestPoint(centroid)
+        centerPt = brepFace.PointAt(uv[1], uv[2])
+        normalVector = brepFace.NormalAt(uv[1], uv[2])
 
-    SurfaceData = namedtuple('SurfaceData', 'center_pt normal_vector')
+    SurfaceData = namedtuple('SurfaceData', 'centerPt normalVector')
 
-    return SurfaceData(center_pt, normal_vector)
+    return SurfaceData(centerPt, normalVector)
 
 
-def check_planarity(hb_surface, tolerance=1e-3):
-    """Check planarity of a hb_surface.
+def checkPlanarity(HBSurface, tolerance=1e-3):
+    """Check planarity of a HBSurface.
 
     Args:
-        hb_surface: A Honeybee surface
+        HBSurface: A Honeybee surface
         tolerance: A float number as tolerance (Default: 1e-3)
 
     Returns:
         True is the surface is planar, otherwise return False.
     """
     try:
-        return hb_surface.geometry.Faces[0].IsPlanar(tolerance)
+        return HBSurface.geometry.Faces[0].IsPlanar(tolerance)
     except AttributeError as e:
-        raise TypeError("Input is not a hb_surface: %s" % str(e))
+        raise TypeError("Input is not a HBSurface: %s" % str(e))
 
 
-def check_for_internal_edge(hb_surface):
+def checkForInternalEdge(HBSurface):
     """Check if the surface has an internal edge.
 
     For surfaces with internal edge surfaces needs to be meshed to extract the points.
 
     Args:
-        hb_surface: A Honeybee surface
+        HBSurface: A Honeybee surface
 
     Returns:
         True is the surface has an internal edge, otherwise return False.
@@ -201,9 +201,9 @@ def check_for_internal_edge(hb_surface):
     # surface is an open brep but since I couldn't find it I'm using this method
     # if Surface has no intenal edges the length of joined border is 1
     try:
-        edges = hb_surface.geometry.DuplicateEdgeCurves(True)
+        edges = HBSurface.geometry.DuplicateEdgeCurves(True)
     except AttributeError as e:
-        raise TypeError("Input is not a hb_surface: %s" % str(e))
+        raise TypeError("Input is not a HBSurface: %s" % str(e))
     else:
         border = rc.Geometry.Curve.JoinCurves(edges)
         if len(border) > 1:
