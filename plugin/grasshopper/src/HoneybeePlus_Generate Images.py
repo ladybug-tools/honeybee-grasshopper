d82d698a60a633ebd9ae8229f8020bab7ca2fdfc
LAPTOP-MB0HU4QI\Mostapha <sadeghipour@gmail.com>
2018-02-07 21:02:35 -0500

    fixed method names

--- plugin/grasshopper/src/HoneybeePlus_Generate Images.py
+++ plugin/grasshopper/src/HoneybeePlus_Generate Images.py
@@ -44,9 +44,9 @@ if _imgCollection:
     assert _mode_ < 4, \
         '_mode_ can only be 0: combined, 1: total, 2: direct or 3: sky.'
 
-    states = _imgCollection.parseBlindStates(blindStates_)
+    states = _imgCollection.parse_blind_states(blindStates_)
     
     print(_imgCollection.details)
     print('Loading {} values for several hours.'.format(_modes[_mode_]))
     
-    images = _imgCollection.generateCombinedImagesById(hoys_, states, _mode_) 
+    images = _imgCollection.generate_combined_images_by_id(hoys_, states, _mode_) 
