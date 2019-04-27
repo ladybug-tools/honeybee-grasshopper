a0be86d47ddd1394e90a1fc0440ebc266ea44d81
Chris Mackey <chris@mackeyarchitecture.com>
2019-04-27 12:59:35 -0400

    Improved Component Description
    ... and I added a line of code since it seems the hoys do not exist when the gird is first loaded up.

--- plugin/grasshopper/src/HoneybeePlus_Hourly Values.py
+++ plugin/grasshopper/src/HoneybeePlus_Hourly Values.py
@@ -16,9 +16,10 @@ Hourly results for an analysis grid for a single hour of the year.
         hoy_: An hour of the year (default: first available hour).
         blind_state_: Blind states for light sources as a tuples. You can use
             If left empty the first state of each window group will be used.
-        _mode_: An integer between 0-2. 0 returns that total values, 1 returns
-            diret values if available and 2 returns sky + diffuse values if
-            available.
+        _mode_: An integer between 0-2 representing the type of value to output.
+            0 = total values
+            1 = direct values (if available)
+            2 = diffuse sky + reflected values (if available).
     Returns:
         report: Reports, errors, warnings, etc.
         values: List of hourly values for each sensor.
@@ -26,7 +27,7 @@ Hourly results for an analysis grid for a single hour of the year.
 
 ghenv.Component.Name = "HoneybeePlus_Hourly Values"
 ghenv.Component.NickName = 'hourlyValues'
-ghenv.Component.Message = 'VER 0.0.05\nOCT_22_2018'
+ghenv.Component.Message = 'VER 0.0.05\nAPR_27_2019'
 ghenv.Component.Category = "HoneybeePlus"
 ghenv.Component.SubCategory = '04 :: Daylight :: Daylight'
 ghenv.Component.AdditionalHelpFromDocStrings = "4"
@@ -41,6 +42,7 @@ except ImportError as e:
 if _analysis_grid:
     _modes = ('total', 'direct', 'diffuse')
     _mode_ = _mode_ or 0
+    _analysis_grid.sum_values_by_id([])
     hoy_ = hoy_ or _analysis_grid.hoys[0]
     assert _mode_ < 3, '_mode_ can only be 0: total, 1: direct or 2: sky.'
 
